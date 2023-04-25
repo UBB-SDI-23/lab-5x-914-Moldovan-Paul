d7883fab1f621085b8dd947b56f79f2c74141fb1
914-Moldovan-Paul <paul.moldovan1@stud.ubbcluj.ro>
2023-04-26 00:00:01 +0300

    a

--- hospital-organizer-frontend/src/patients/AddPatient.js
+++ hospital-organizer-frontend/src/patients/AddPatient.js
@@ -21,7 +21,7 @@ export default function AddPatient() {
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    await axios.post("../../patients", patient);
+    await axios.post("../../api/patients", patient);
     navigate("/");
   };
 
